82f40a70aeb2a44e221a280e96b8e450d2f67e1f
Jonathan <jonathancjonsson@gmail.com>
2025-03-14 15:03:57 +0100

    Implement grid

--- index.js
+++ index.js
@@ -5,6 +5,8 @@ let gridSize = 16;
 function createSquare() {
 	const square = document.createElement("div");
 	square.classList.add("square");
+	gridContainer.classList.add("grid");
+	square.style.width = `calc(100% / ${gridSize})`;
 	gridContainer.appendChild(square);
 }
 
